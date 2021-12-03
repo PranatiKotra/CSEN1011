5658996a6389cc92515eb526bd84d09471220988
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-03 12:05:10 +0530

    Create add-two-num.c

---
+++ add-two-num.c
@@ -0,0 +1,27 @@
+/*
+Algorithm:
+1. Start.
+2. Enter two numbers a and b.
+3. sum=a+b.
+4. Print 
+5. Stop.
+*/
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+  printf("Enter two integers:\n");
+	scanf("%d %d\n",&a,&b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
+/*
+Output:
+Enter two integers
+5 6
+11
+
+Learnings:
+1.Finding sum of integers.
+*/
