057916bca509beef785f3bd5dcc03346308914d2
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-07 14:01:36 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int n,n1,s=0,r;
+  printf("Enter a three digit number:");
+  scanf("%d",&n);
+  n1=n;
+  while(n1!=0){
+    r=n1%10;
+    s=s+(r*r*r);
+    n1/=10;
+  }
+  if(n==s){
+    printf("%d is an ARMSTRONG number",s);
+  }
+  else{
+    printf("%d is NOT an ARMSTRONG number.",s);
+  }
+  return 0;
+}
