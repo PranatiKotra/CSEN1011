8ee98e2c8e8d08ad4b76126d357214a0ea56a533
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-16 14:30:42 +0530

    Create netsal.c

---
+++ netsal.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+
+	float basic, da, hra, ta, others;
+	float pf,it;
+	float net_salary;
+	
+	printf("Enter Basic Salary ($): ");
+	scanf("%f",&basic);
+	printf("Enter HRA ($): ");
+	scanf("%f",&hra);
+	printf("Enter TA ($): ");
+	scanf("%f",&ta);	
+	printf("Enter others ($): ");
+	scanf("%f",&others);	
+	
+	da = (basic*12)/100;
+	pf = (basic*14)/100;
+	it = (basic*15)/100;
+	
+	net_salary = basic + da + hra + ta + others - (pf+it);
+	
+	printf("Net Salary is: $ %.02f\n",net_salary);
+	
+	return 0;
+}
+/*
+Output: 
+Enter Basic Salary ($): 600
+Enter HRA ($): 500
+Enter TA ($): 20
+Enter others ($): 600
+Net Salary is: $ 1618.00
+*/
