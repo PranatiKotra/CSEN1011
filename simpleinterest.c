38f7e85ec782cfc10d6ec085d0c40a5905b3811d
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-16 13:51:56 +0530

    Create simpleinterest.c

---
+++ simpleinterest.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main() {
+  int p, t, r;
+  
+  printf("Principle = " );
+  scanf("%d", &p);
+
+  printf("Rate of Interest = " );
+  scanf("%d", &r);
+
+  printf("Time period = " );
+  scanf("%d", &t);
+
+  printf("Simple Interest = %d \n", (p * t * r / 100) );
+
+  return 0;
+} 
+/*
+Output:
+Principle=100
+Rate of Interest=50
+Time period=2
+Simple Interest=100
+*/    
