facbb162a26dfbe8094a816734f70066f3388db2
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-23 14:50:39 +0530

    Update angles.c

--- angles.c
+++ angles.c
@@ -1,16 +1,8 @@
-/*
-Algorithm:
-1. Start.
-2. Enter two angles of triangle a and b.
-3. third angle=180-(a+b).
-4. Print third angle.
-5. Stop.
-*/
 #include<stdio.h>
 int main()
 {
 	int a1,a2,a3;
-  printf("Enter two angles:\n");
+        printf("Enter two angles:\n");
 	scanf("%d %d\n",&a1,&a2);
 	a3=180-a1-a2;
 	printf("%d",a3);
@@ -21,7 +13,4 @@ Output:
 Enter two angles
 30 60
 90
-
-Learnings:
-1.Finding the third angle of a triangle.
 */
