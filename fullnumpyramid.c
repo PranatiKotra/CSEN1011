57a01f26f80ace3f52c471340da15cab2ed5347b
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2021-12-23 14:22:27 +0530

    Update fullnumpyramid.c

--- fullnumpyramid.c
+++ fullnumpyramid.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 int main() {
-   int i, s, r, k = 0, c = 0, c1 = 0;
-   printf("Enter the number of rows: ");
-   scanf("%d", &r);
-   for (i = 1; i <= r; ++i) {
-      for (s = 1; s <= r - i; ++s) {
-         printf("  ");
-         ++c;
-      }
-      while (k != 2 * i - 1) {
-         if (c <= r - 1) {
-            printf("%d ", i + k);
-            ++c;
-         } else {
-            ++c1;
-            printf("%d ", (i + k - 2 * c1));
-         }
-         ++k;
-      }
-      c1 = c = k = 0;
-      printf("\n");
-   }
-   return 0;
+  int i,j,k,s,n;
+  printf("Enter number of rows: ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++) {
+    for(s=1;s<=n-i;s++) {
+      printf(" ");
+    }
+    k=i;
+    for(j=1;j<=i;j++) {
+      printf("%d ",k++);
+    }
+    k=k-2;
+    for(j=1;j<i;j++) {
+      printf("%d ",k--);
+    }
+    printf("\n");
+  }
+  return 0;
 }
 /*
 output:
