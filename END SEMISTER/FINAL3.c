50b16fa14c7ebc1d3fb8ab081160155531162454
PranatiKotra <94438529+PranatiKotra@users.noreply.github.com>
2022-02-25 12:09:40 +0530

    Update FINAL3.c

--- END SEMISTER/FINAL3.c
+++ END SEMISTER/FINAL3.c
@@ -1,52 +1,37 @@
 #include<stdio.h>
-int main()
-{
-    int i;
-    struct customer
-    {
-        char name[15];
-        int amount;
-        int mobile_number[10];
-        
-    }c1,c2;
+int main(){
+	struct customer {
 
-	//printf("enter the name of the customer 1");
-	scanf("%s",c1.name);
-	//printf("enter the amount of customer 1");
-	scanf("%d",&c1.amount);
-	//printf("enter the number of the customer 1");
-	for(i=1;i<=10;i++)
-	{
-		scanf("%d",&c1.mobile_number[i]);
+	char name[15];
+
+	int amount;
+
+	char mobile_number[10];
+
+	}a[2];
+	for(int i=0;i<2;i++){
+	scanf("%s",a[i].name);
+	scanf("%d",&a[i].amount);
+	if(n[i].amount<0){
+		printf("Invalid amount");
+	return 0;
 	}
-	//printf("enter the name of the customer 2");
-	scanf("%s",c2.name);
-	//printf("enter the amount of customer 2");
-	scanf("%d",&c2.amount);
-	//printf("enter the number of the customer 2");
-	for(i=1;i<=10;i++)
-	{
-		scanf("%d",&c2.mobile_number[i]);
+	scanf("%s",a[i].mobile_number);
+	if(strlen(a[i].mobile_number)!=10){
+		printf("Invalid mobile number");
+	return 0;
 	}
-	if(i!=10)
-	{
-    	printf("Invalid phone number");
+	int j=0;
+	while(a[i].mobile_number[j]!='\0'){
+		if(a[i].mobile_number[j]>='0' && a[i].mobile_number[j]<='9')
+		j+=1;
 	}
-	if(c1.amount<0 || c2.amount<0)
-	{
-    	printf("Invalid amount");
+	else{
+		printf("Invalid entry");
+		return 0;
 	}
-	else
-	{
-    	printf("%d",c1.amount);
-	}
-	if((c1.mobile_number>=97 && c1.mobile_number<=122)||(c1.mobile_number>=65 && c1.mobile_number<=90)){
-		printf("Invalid Entry");
-	}
-	if((c2.mobile_number>=97 && c2.mobile_number<=122)||(c2.mobile_number>=65 && c2.mobile_number<=90)){
-		printf("Invalid Entry");
-	}
-	//c2.amount=c1.amount;
-	printf("total amount = %d ",c1.amount+c2.amount);
+	int total;
+	total=a[0].amount+a[1].amount;
+	printf("%d",total);
 	return 0;
 }
